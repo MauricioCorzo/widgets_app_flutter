3495746e678bda079d89f65e1687c94b504fecd1
MauricioCorzo <mauricio.corzo@yahoo.com>
2024-09-12 18:38:23 -0300

    image card example

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -34,8 +34,18 @@ class _CardsView extends StatelessWidget {
         children: [
           ...cardsData.map((card) => _CustomCardType1(
               label: card["label"], elevation: card["elevation"])),
-          ...cardsData.map((card) => _CustomCardType1(
-              label: card["label"], elevation: card["elevation"]))
+          ...cardsData.map((card) => _CustomCardType2(
+              label: card["label"], elevation: card["elevation"])),
+          ...cardsData.map((card) => _CustomCardType3(
+              label: card["label"], elevation: card["elevation"])),
+          ...cardsData.map((card) => Align(
+                alignment: Alignment.centerRight,
+                child: _CustomCardType4(
+                    label: card["label"], elevation: card["elevation"]),
+              )),
+          const SizedBox(
+            height: 20,
+          )
         ],
       ),
     );
@@ -51,11 +61,12 @@ class _CustomCardType1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      color: Colors.indigo[100],
       elevation: this.elevation,
       child: SizedBox(
         width: double.infinity,
         child: Padding(
-          padding: const EdgeInsets.fromLTRB(10, 10, 10, 10),
+          padding: const EdgeInsets.fromLTRB(10, 5, 0, 5),
           child: Column(
             children: [
               Align(
@@ -65,9 +76,173 @@ class _CustomCardType1 extends StatelessWidget {
                   icon: const Icon(Icons.more_vert),
                 ),
               ),
-              Align(alignment: Alignment.bottomLeft, child: Text(this.label)),
+              Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text('Normal Color - ${this.label}')),
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomCardType2 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CustomCardType2({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: const BorderRadius.all(Radius.circular(8)),
+        side: BorderSide(color: colors.outline),
+      ),
+      elevation: this.elevation,
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 5, 0, 5),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.more_vert),
+                ),
+              ),
+              Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text('Border - ${this.label}')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomCardType3 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CustomCardType3({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    final colors = Theme.of(context).colorScheme;
+    return Card(
+      color: colors.surfaceContainerHighest,
+      elevation: this.elevation,
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 5, 0, 5),
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.more_vert),
+                ),
+              ),
+              Align(
+                  alignment: Alignment.bottomLeft,
+                  child: Text('Filled - ${this.label}')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _CustomCardType4 extends StatelessWidget {
+  final String label;
+  final double elevation;
+
+  const _CustomCardType4({required this.label, required this.elevation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      elevation: this.elevation,
+      child: SizedBox(
+        // width: double.infinity, // PARA QUE FUNCIONE EL ALIGN DEL PADRE EN EL MAP
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            DecoratedBox(
+              position: DecorationPosition.foreground,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Colors.transparent, Colors.black54],
+                    stops: [0.5, 1],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter),
+              ),
+              child: Image.network(
+                'https://picsum.photos/id/${this.elevation.toInt() + 700}/300/200',
+                height: 200,
+                // width: double.infinity,
+                alignment: Alignment.center,
+                fit: BoxFit.cover,
+              ),
+            ),
+            // const Positioned.fill(
+            //   child: DecoratedBox(
+            //     decoration: BoxDecoration(
+            //       gradient: LinearGradient(
+            //           colors: [Colors.transparent, Colors.black],
+            //           stops: [0.7, 1],
+            //           begin: Alignment.topCenter,
+            //           end: Alignment.bottomCenter),
+            //     ),
+            //   ),
+            // ),
+            Positioned.fill(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                              colors: [Colors.white, Colors.black],
+                              stops: [0.5, 0.6],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight),
+                          color: Colors.white,
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(20))),
+                      child: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.more_vert),
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: const Alignment(-0.95, 0),
+                    child: Text(
+                      'Image - ${this.label}',
+                      style: const TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
